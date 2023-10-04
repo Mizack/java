e853a1ccf4ac052fb80c221220f7406329f56d9c
mizack mianuti <mizack1234@gmail.com>
2023-10-03 21:45:39 -0300

    testando coisas

--- src/main/java/com/miza/miza/MizaApplication.java
+++ src/main/java/com/miza/miza/MizaApplication.java
@@ -2,11 +2,13 @@ package com.miza.miza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MizaApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure().load();
 		SpringApplication.run(MizaApplication.class, args);
 	}
 
